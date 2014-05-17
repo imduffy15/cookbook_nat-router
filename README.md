a4de8ee46c2df3d11c62882b0fe09a26de57cb31
Ian Duffy <ian@ianduffy.ie>
2014-05-17 14:51:21 +0100

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+nat-router Cookbook
+===================
+
+Configures iptables to act as a NAT router between two interfaces
+
+Requirements
+------------
+
+Should work on any Red Hat-family or Debian-family Linux distribution.
+
+
+Usage
+-----
+
+TODO
+
+
+Attributes
+----------
+
+TODO
+
+
+LICENSE AND AUTHOR
+==================
+
+- Authors:: Ian Duffy (<ian@ianduffy.ie>)
+
+```text
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ recipes/default.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: nat-router
+# Recipe:: default
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package 'iptables'
+
+template '/etc/sysctl.d/60-network-forwarding.conf' do
+  source 'network-forwarding.conf.erb'
+  mode 0644
+  owner 'root'
+  group 'root'
+end
+
+execute 'iptables-restore' do
+  command '/sbin/iptables-restore < /etc/network/iptables'
+  action :nothing
+end
+
+template '/etc/network/iptables' do
+  source 'iptables.erb'
+  mode 0644
+  owner 'root'
+  group 'root'
+  notifies :run, resources(:execute => 'iptables-restore')
+end
+
+template '/etc/network/if-pre-up.d/iptables-restore' do
+  source 'iptables-restore.erb'
+  mode 0755
+  owner 'root'
+  group 'root'
+end
