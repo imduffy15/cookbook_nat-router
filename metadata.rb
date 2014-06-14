10b6ef9e6dd8b4d686814f83859755ac6a173ccd
Ian Duffy <ian@ianduffy.ie>
2014-06-14 22:19:35 +0100

    Add vagrantfile for testing purposes

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,9 @@
-name "nat-router"
-maintainer "Ian Duffy"
-maintainer_email "ian@ianduffy.ie"
-license "Apache 2.0"
-description "Configures iptables as a nat firewall"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.1"
+name 'nat-router'
+maintainer 'Ian Duffy'
+maintainer_email 'duffy@apache.org'
+license 'Apache 2.0'
+description 'Installs/Configures iptables as a nat router'
+long_description 'Installs/Configures iptabes as a nat router'
+version '0.0.1'
 
-%w{ redhat centos scientific amazon }.each do |os|
-  supports os
-end
-
-recipe "nat-router", "Configures iptables as a nat firewall"
+recipe "nat-router::default", "Installs and Configures iptables as a nat router."
